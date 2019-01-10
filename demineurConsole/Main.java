6e43a2e77db001e4f81498e6b5bdfe86a8e018de
Guillaume <g.rossinot@gmail.com>
2019-01-10 16:49:56 +0100

    demineur console

---
+++ demineurConsole/Main.java
@@ -0,0 +1,12 @@
+package demineurConsole;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DemTest  DT = new DemTest(9, 9, 10);
+		DT.Jouer();
+
+	}
+
+}
